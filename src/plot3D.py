8c41861160e53ef710a9c0604e6d7ea880a8abfd
RikHollebrandse <152167478+RikHollebrandse@users.noreply.github.com>
2023-12-06 13:40:25 +0100

    update

--- src/plot3D.py
+++ src/plot3D.py
@@ -7,16 +7,16 @@ from multiple_linear_regression import MultipleLinearRegressor
 # Import RegressionPlotter from regression_plotter.py
 from regression_plotter import RegressionPlotter
 
-# Example data for 2D regression
-X = np.array([[1], [2], [3], [4], [5]])  # Single feature
-y = np.array([2, 3, 5, 7, 11])           # Target values
+# Example data for 3D regression
+X = np.array([[1, 1], [1, 2], [2, 2], [2, 3], [3, 3]])  # Two features
+y = np.array([2, 3, 3, 5, 5])                           # Target values
 
 # Initialize the MultipleLinearRegression model and train it
 model = MultipleLinearRegressor()
 model.train(X, y)
 
-# Create an instance of RegressionPlotter and plot 2D regression
+# Create an instance of RegressionPlotter and plot 3D regression
 plotter = RegressionPlotter(model, X, y)
-plotter.plot()  # This will generate a 2D plot as we have only one feature
+plotter.plot()  # This will generate a 3D plot as we have two features
 
 

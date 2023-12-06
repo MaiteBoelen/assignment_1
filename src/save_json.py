fd7b1bbf4118779ca5ce94523c4658f21b5d0e45
MaiteBoelen <152171228+MaiteBoelen@users.noreply.github.com>
2023-12-06 15:18:44 +0100

    Create save_json.py
    save json versie 1

---
+++ src/save_json.py
@@ -0,0 +1,21 @@
+from multiple_linear_regression import MultipleLinearRegression
+from model_saver import ModelSaver
+import json
+
+model = MultipleLinearRegression
+# Create an instance of ModelSaver
+saver = ModelSaver(format_type='json')
+
+# Save the model parameters
+saver.save_model(model, 'saved_model.json')
+
+# Create a new instance of SimpleModel
+new_model = MultipleLinearRegression()
+
+# Load the model parameters to the new model
+saver.load_model(new_model, 'saved_model.json')
+
+# Check if the parameters are successfully loaded
+print("Original Model Parameters -- Intercept:", model.intercept, "Coefficients:", model.coefficients)
+print("Loaded Model Parameters -- Intercept:", new_model.intercept, "Coefficients:", new_model.coefficients)
+
